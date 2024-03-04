aea6997a8b5dd3bd4e8d7d1b660e7c79c4e302f1
John Jimenez <jjimenez19961@gmail.com>
2024-03-04 08:40:40 -0500

    fix image for properties

--- app/javascript/src/property/property.jsx
+++ app/javascript/src/property/property.jsx
@@ -46,7 +46,7 @@ class Property extends React.Component {
       user,
     } = property
 
-    const imageUrl = `https://jj-fswd-airbnb-clone-6bde614cb3be.herokuapp.com/${image}`;
+    const imageUrl = `https://jj-fswd-airbnb-clone-6bde614cb3be.herokuapp.com${image}`;
 
 
     return (
